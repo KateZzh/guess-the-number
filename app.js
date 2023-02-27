42e26b6acfe1bf0ae8c6bd06bbc704b85ab2ecdf
KateZzh <ekaterin.pro2@gmail.com>
2023-02-27 22:10:45 +0300

    fix

--- app.js
+++ app.js
@@ -12,37 +12,44 @@
 const rndNum = Math.round(Math.random() * 100);
 
 function guessNum(rndNum_) {
-  let start = 0;
-  let end = 100;
-  let countOfIteration = 0;
-  alert("Программа загадала рандомное число. Ваша задача угадать его.");
+  try {
+    let start = 0;
+    let end = 100;
+    let countOfIteration = 0;
+    alert("Программа загадала рандомное число. Ваша задача угадать его.");
 
-  for (let i = start; i < end; i++) {
-    let userNum = prompt(`Введите число от ${start} до ${end}`);
+    for (let i = start; i < end; i++) {
+      let userNum = prompt(`Введите число от ${start} до ${end}`);
 
-    if (userNum == null) {
-      alert("Вы закончили игру");
-      break;
-    }
-
-    if (userNum >= start && userNum <= end) {
-      
-      if (userNum == rndNum_) {
-        alert(`Вы угадали число за ${countOfIteration} итераций!`);
+      if (userNum == null) {
+        alert("Вы закончили игру");
         break;
-      } else if (userNum < rndNum_) {
-        countOfIteration++;
-        start = userNum;
-        alert(`Вы ввели число ${userNum} и это слишком маленькое значение. Введите число от ${start} до ${end}`);
-      } else if (userNum > rndNum_) {
-        countOfIteration++;
-        end = userNum;
-        alert(`Вы ввели число ${userNum} и это слишком большое значение. Введите число от ${start} до ${end}`);
       }
 
-    } else {
-      userNum = prompt(`Введите число от ${start} до ${end}`);
+      if (userNum >= start && userNum <= end) {
+        if (userNum == rndNum_) {
+          alert(`Вы угадали число за ${countOfIteration} итераций!`);
+          break;
+        } else if (userNum < rndNum_) {
+          countOfIteration++;
+          start = userNum;
+          alert(
+            `Вы ввели число ${userNum} и это слишком маленькое значение. Введите число от ${start} до ${end}`
+          );
+        } else if (userNum > rndNum_) {
+          countOfIteration++;
+          end = userNum;
+          alert(
+            `Вы ввели число ${userNum} и это слишком большое значение. Введите число от ${start} до ${end}`
+          );
+        }
+      } else {
+        userNum;
+      }
+      
     }
+  } catch (error) {
+    return error.message;
   }
 }
 
