f379c312aa0baf5fa4e5317ecb317be934131fa7
KateZzh <ekaterin.pro2@gmail.com>
2023-03-22 23:41:55 +0300

    using OOP

--- app.js
+++ app.js
@@ -9,49 +9,46 @@
 // Целые числа; Переменные; Ввод / вывод данных; Условный оператор; Цикл; Бесконечный цикл;
 // Операторы break, continue; Работа с модулем random для генерации случайных чисел; Функции.
 
-const rndNum = Math.round(Math.random() * 100);
+class RandomNumber {
+  guessNum(rndNum_) {
+    try {
+      let start = 0;
+      let end = 100;
+      let countOfIteration = 0;
+      alert("Программа загадала рандомное число. Ваша задача угадать его.");
 
-function guessNum(rndNum_) {
-  try {
-    let start = 0;
-    let end = 100;
-    let countOfIteration = 0;
-    alert("Программа загадала рандомное число. Ваша задача угадать его.");
+      for (let i = start; i < end; i++) {
+        let userNum = prompt(`Введите число от ${start} до ${end}`);
 
-    for (let i = start; i < end; i++) {
-      let userNum = prompt(`Введите число от ${start} до ${end}`);
-
-      if (userNum == null) {
-        alert("Вы закончили игру");
-        break;
-      }
-
-      if (userNum >= start && userNum <= end) {
-        if (userNum == rndNum_) {
-          alert(`Вы угадали число за ${countOfIteration} итераций!`);
+        if (userNum == null) {
+          alert("Вы закончили игру");
           break;
-        } else if (userNum < rndNum_) {
-          countOfIteration++;
-          start = userNum;
-          alert(
-            `Вы ввели число ${userNum} и это слишком маленькое значение. Введите число от ${start} до ${end}`
-          );
-        } else if (userNum > rndNum_) {
-          countOfIteration++;
-          end = userNum;
-          alert(
-            `Вы ввели число ${userNum} и это слишком большое значение. Введите число от ${start} до ${end}`
-          );
         }
-      } else {
-        userNum;
+
+        if (userNum >= start && userNum <= end) {
+          if (userNum == rndNum_) {
+            alert(`Вы угадали число за ${countOfIteration} итераций!`);
+            break;
+          } else if (userNum < rndNum_) {
+            countOfIteration++;
+            start = userNum;
+            alert(`Вы ввели число ${userNum} и это слишком маленькое значение. Введите число от ${start} до ${end}`);
+          } else if (userNum > rndNum_) {
+            countOfIteration++;
+            end = userNum;
+            alert(`Вы ввели число ${userNum} и это слишком большое значение. Введите число от ${start} до ${end}`);
+          }
+        } else {
+          userNum;
+        }
       }
-      
+    } catch (error) {
+      return error.message;
     }
-  } catch (error) {
-    return error.message;
   }
 }
 
-const result = guessNum(rndNum);
-console.log(result);
+const rndNum = Math.round(Math.random() * 100);
+
+const randomNumber = new RandomNumber();
+randomNumber.guessNum(rndNum);
